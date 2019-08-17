f8d3a118b6a9fa5fd0e2da0c149991d53ca748ed
MatheusVilas <mats.vilas@gmail.com>
2019-08-16 23:04:02 -0300

    Blog

---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  render() {
+    return (
+      <header>
+        <nav>
+          vilasboas.dev
+        </nav>
+      </header>
+    )
+  }
+}
+
+export default Header
---
+++ src/views/home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Header from "../../components/header"
+export class index extends Component {
+  render() {
+    return (
+      <>
+        <Header/> 
+      </>
+    )
+  }
+}
+
+export default index
